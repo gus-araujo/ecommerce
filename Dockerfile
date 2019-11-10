c5b1b8fd4b38f19c9510169a1b146de27868c1b5
gus-araujo <gus.araujo@enjoei.com.br>
2019-11-10 03:27:03 -0300

    add some stuff to index

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM ruby:2.5.1
+ENV NAME="Unknown Ecommerce"
 RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
 RUN mkdir /ecommerce
 WORKDIR /ecommerce
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191027233540) do
+ActiveRecord::Schema.define(version: 20191110061259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
-    t.string   "name"
+    t.string   "title"
     t.float    "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
